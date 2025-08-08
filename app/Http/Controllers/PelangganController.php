2995dfd6cf98a8f6c63ab0fc9de33d5f6f0974d9
rizaoktorabp <rizaoktora22@gmail.com>
2025-08-08 07:26:07 +0700

    buat controller untuk pelanggan

--- app/Http/Controllers/PelangganController.php
+++ app/Http/Controllers/PelangganController.php
@@ -2,16 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Pelanggan;
 use Illuminate\Http\Request;
+use function PHPUnit\Framework\returnArgument;
 
 class PelangganController extends Controller
 {
     public function index()
     {
-        // Di sini Anda bisa mengambil data pelanggan dari database
-        // $customers = Customer::all();
-        // return view('customers.index', compact('customers'));
-        return view('pelanggan.index');
+        $pelanggan = Pelanggan::all();
+
+        return view('pelanggan.index', compact('pelanggan'));
     }
 
     public function create()
@@ -41,30 +42,15 @@ class PelangganController extends Controller
     public function show($id)
     {
         // Data statis untuk preview detail pelanggan
-        $data = [
-            'no_internet' => '152516209310',
-            'no_digital' => '152516209310',
-            'tanggal_ps' => '08/07/2023',
-            'kecepatan' => '200',
-            'bulan' => '7',
-            'tahun' => '2025',
-            'datel' => 'BNYWANGI',
-            'ro' => '',
-            'sto' => 'RGJ',
-            'nama' => 'Toko Rofi / MOH ROFIUDIN',
-            'segmen' => 'DBS-Commerce & Community Serv',
-            'kcontact' => 'DS/05/JR/DS50205/MUHLAS/DIGIBIZ 75MBPS/PIC 81217766672',
-            'jenis_layanan' => 'INDIBIZ',
-            'channel_1' => 'Sales Force DBS',
-            'cek_netmonk' => '',
-            'cek_pijar_mahir' => '',
-            'cek_eazy_cam' => '',
-            'cek_oca' => '',
-            'cek_pijar_sekolah' => '',
-            'kode_sales' => 'DS50216',
-            'nama_sf' => 'RYZAL RYAN (BLM)',
-            'agency' => 'MCA',
-        ];
-        return view('pelanggan.show', compact('data'));
+        $data = Pelanggan::findOrFail($id);
+
+        return view('pelanggan.show', ['pelanggan' => $data]);
+    }
+
+    public function edit($id)
+    {
+        // Data statis untuk halaman edit
+        $data = Pelanggan::findOrFail($id);
+        return view('pelanggan.edit', ['pelanggan' => $data]);
     }
 }

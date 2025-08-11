af7e577d0c9382543733cb2d18ca282a29a07f6d
rizaoktorabp <rizaoktora22@gmail.com>
2025-08-12 05:45:19 +0700

    mengubah store dan update pelanggan

--- app/Http/Controllers/PelangganController.php
+++ app/Http/Controllers/PelangganController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Pelanggan\ImportPelangganRequest;
+use App\Http\Requests\Pelanggan\UpdatePelangganRequest;
+use App\Imports\PelangganImport;
 use App\Models\Pelanggan;
 use Illuminate\Http\Request;
+use Maatwebsite\Excel\Facades\Excel;
 use function PHPUnit\Framework\returnArgument;
 
 class PelangganController extends Controller
@@ -20,20 +24,15 @@ class PelangganController extends Controller
         return view('pelanggan.create');
     }
 
-    public function store(Request $request)
+    public function store(ImportPelangganRequest $request)
     {
-        // Validasi data
-        $validated = $request->validate([
-            'no_internet' => 'required|string|max:50',
-            'no_digital' => 'required|string|max:50',
-            'tanggal_ps' => 'required|date',
-            'datel' => 'required|string|max:50',
-            'sto' => 'required|string|max:50',
-            'nama' => 'required|string|max:100',
-        ]);
+        // // Ambil data yang sudah divalidasi dari Form Request
+        // $validated = $request->validated();
 
-        // Simpan data ke database
-        // Customer::create($validated);
+        // // Simpan data ke database
+        // Pelanggan::create($validated);
+
+        Excel::import(new PelangganImport, $request->file('file_upload'));
 
         // Redirect dengan pesan sukses
         return redirect()->route('pelanggan.index')->with('success', 'Data pelanggan berhasil ditambahkan');
@@ -52,5 +51,28 @@ class PelangganController extends Controller
         // Data statis untuk halaman edit
         $data = Pelanggan::findOrFail($id);
         return view('pelanggan.edit', ['pelanggan' => $data]);
+    }
+
+    public function update(UpdatePelangganRequest $request, $id)
+    {
+        $pelanggan = Pelanggan::findOrFail($id);
+        $pelanggan->update($request->validated());
+
+        return redirect()
+            ->route('pelanggan.index')
+            ->with('success', 'Data pelanggan berhasil diperbarui.');
+    }
+
+    public function destroy($id)
+    {
+        // Cari data berdasarkan ID
+        $pelanggan = Pelanggan::findOrFail($id);
+
+        // Hapus data
+        $pelanggan->delete();
+
+        // Redirect kembali dengan pesan sukses
+        return redirect()->route('pelanggan.index')
+            ->with('success', 'Data pelanggan berhasil dihapus');
     }
 }

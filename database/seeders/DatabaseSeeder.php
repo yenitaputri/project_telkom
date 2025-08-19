d764382efa46487b7216515afce9ff219106e0a8
rizaoktorabp <rizaoktora22@gmail.com>
2025-08-19 07:04:18 +0700

    buat seeder untuk prodigi

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
-            // PelangganSeeder::class
+                // PelangganSeeder::class,
+            ProdigiSeeder::class
         ]); // ✅ jalankan UserSeeder, jangan buat data manual
     }
 
---
+++ database/seeders/ProdigiSeeder.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Prodigi;
+use Illuminate\Database\Seeder;
+
+class ProdigiSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $data = [
+            [
+                'order_id' => '0123456789',
+                'nd' => '87654321',
+                'customer_name' => 'Budi Santoso',
+                'witel' => 'Jakarta Pusat',
+                'telda' => '021-1234567',
+                'produk' => 'Indihome 1P',
+                'tanggal_ps' => '2024-08-18',
+            ],
+            [
+                'order_id' => '0123456790',
+                'nd' => '98765432',
+                'customer_name' => 'Siti Aminah',
+                'witel' => 'Bandung Selatan',
+                'telda' => '022-9876543',
+                'produk' => 'Indihome 2P',
+                'tanggal_ps' => '2024-08-17',
+            ],
+            [
+                'order_id' => '0123456791',
+                'nd' => '12345678',
+                'customer_name' => 'Joko Susilo',
+                'witel' => 'Surabaya Barat',
+                'telda' => '031-2345678',
+                'produk' => 'Indihome 3P',
+                'tanggal_ps' => '2024-08-16',
+            ],
+        ];
+
+        foreach ($data as $item) {
+            Prodigi::create($item);
+        }
+    }
+}

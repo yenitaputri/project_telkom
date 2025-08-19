ab9e9b7d7159ab106a2ee96e63a7ec2fb47e0ddb
rizaoktorabp <rizaoktora22@gmail.com>
2025-08-19 07:04:04 +0700

    buat model dan migrasi untuk prodigi

---
+++ app/Models/Prodigi.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Prodigi extends Model
+{
+    use HasFactory;
+
+    protected $table = 'prodigi';
+
+    protected $fillable = [
+        'order_id',
+        'nd',
+        'customer_name',
+        'witel',
+        'telda',
+        'produk',
+        'tanggal_ps',
+        'rev',
+        'device',
+    ];
+}
---
+++ database/migrations/2025_08_18_233655_create_prodigis_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('prodigi', function (Blueprint $table) {
+            $table->id();
+            $table->string('order_id')->nullable();
+            $table->string('nd')->nullable();
+            $table->string('customer_name');
+            $table->string('witel')->nullable();
+            $table->string('telda')->nullable();
+            $table->string('produk')->nullable();
+            $table->date('tanggal_ps')->nullable();
+            $table->string('rev')->nullable();
+            $table->string('device')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('prodigis');
+    }
+};

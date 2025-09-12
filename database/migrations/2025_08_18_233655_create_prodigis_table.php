fe064713a71d541a27d407653de6bb2be1564c85
rizaoktorabp <rizaoktora22@gmail.com>
2025-09-12 09:23:45 +0700

    ubah type data witel

--- database/migrations/2025_08_18_233655_create_prodigis_table.php
+++ database/migrations/2025_08_18_233655_create_prodigis_table.php
@@ -4,7 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
@@ -18,7 +19,7 @@ return new class extends Migration {
             $table->string('telda')->nullable();
             $table->string('customer_name')->nullable();
             $table->string('paket')->nullable();
-            $table->date('witel')->nullable();
+            $table->string('witel')->nullable();
             $table->string('rev')->nullable();
             $table->string('device')->nullable();
             $table->timestamps();
